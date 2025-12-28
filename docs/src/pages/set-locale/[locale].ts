5acd08383cc7503c6c374367d31f91cbf43582af
Fadhli Ari Pratama <fadhli.a.pratama101@gmail.com>
2025-12-28 15:20:58 +0700

    refactor: routing Astro model

--- docs/src/pages/set-locale/[locale].ts
+++ docs/src/pages/set-locale/[locale].ts
@@ -1,12 +1,21 @@
 import type { APIRoute } from 'astro'
 
+export async function getStaticPaths() {
+  return [
+    { params: { locale: 'id' } },
+    { params: { locale: 'en' } },
+  ]
+}
+
 export const GET: APIRoute = ({ params, cookies, redirect }) => {
   const locale = params.locale === 'en' ? 'en' : 'id'
+
   cookies.set('preferred_locale', locale, {
     path: '/',
     httpOnly: false,
     sameSite: 'lax',
     maxAge: 60 * 60 * 24 * 365, // 1 tahun
   })
+
   return redirect(`/${locale}`, 302)
 }
